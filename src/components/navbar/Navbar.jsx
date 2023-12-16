e7e2adc73de2c8aa7375f1259c2f92710af870db
Huyh Huyhu <2509roblox@gmail.com>
2023-12-16 08:42:24 +0700

    layout components

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import styles from "./navbar.module.css";
+
+const links = [
+    {
+        id: 1,
+        title: "Home",
+        url: "/",
+    },
+    {
+        id: 2,
+        title: "Portfolio",
+        url: "/portfolio",
+    },
+    {
+        id: 3,
+        title: "Blog",
+        url: "/blog",
+    },
+    {
+        id: 4,
+        title: "About",
+        url: "/about",
+    },
+    {
+        id: 5,
+        title: "Contact",
+        url: "/contact",
+    },
+    {
+        id: 6,
+        title: "Dashboard",
+        url: "/dashboard",
+    },
+];
+
+const Navbar = () => {
+
+    return (
+        <div className={styles.container}>
+            <Link href="/" className={styles.logo}>
+                lamamia
+            </Link>
+            <div className={styles.links}>
+                {links.map((link) => (
+                    <Link key={link.id} href={link.url} className={styles.link}>
+                        {link.title}
+                    </Link>
+                ))}
+
+                <button className={styles.logout} >
+                    Logout
+                </button>
+
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
