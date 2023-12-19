cd13b7d96d63481e9f4623c032e7c19933841c87
Huyh Huyhu <2509roblox@gmail.com>
2023-12-19 07:25:01 +0700

    Next.js Fetch Data on the Client Side (SWR)

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import useSWR from 'swr';
 
 const Dashboard = () => {
+    const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+    const { data, error, isLoading } = useSWR("http://localhost:3000/data.json", fetcher)
+
+    console.log(data)
     return (
         <div>Dashboard</div>
     )
